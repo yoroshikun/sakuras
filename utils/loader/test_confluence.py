6a4d161cc2191afa109d31f8c72340d79a84d28b
Sergei Podlipaev <s-podlipaev@mercari.com>
2023-04-20 17:36:53 +0900

    add draft pages fetcher

--- utils/loader/test_confluence.py
+++ utils/loader/test_confluence.py
@@ -1,12 +1,28 @@
 import unittest
-from confluence import load
+
+from confluence import load, iterate_spaces, iterate_latest_pages
+
 
 class TestChromaUtils(unittest.TestCase):
     def test_load(self):
         documents = load(space_key="~637b97702acfad92d7b0ab23")
         print(documents)
         self.assertEqual(1, 1)
-        
+
+
+class TestFetchConfluence(unittest.TestCase):
+    def test_get_spaces(self):
+        spaces = iterate_spaces()
+        print(spaces)
+        self.assertEqual(1, 1)
+
+
+class TestFetchConfluenceByCQL(unittest.TestCase):
+    def test_exec_cql(self):
+        spaces = iterate_latest_pages()
+        print(spaces)
+        self.assertEqual(1, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
